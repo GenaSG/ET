3961de88530c8ab529cbe625f9ff40f45afe31cd
GenaSG <gennadiy.shvetsov@gmail.com>
2012-12-24 22:51:05 +0200

    fix for mpbin build script

--- wet-sdk/src/makeMP_bin.sh
+++ wet-sdk/src/makeMP_bin.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 BUILD_DIR=./build
 FILES_TO_COMPRESS=""
+MPBINDIR=./mp_bin
 set -x
 pushd $BUILD_DIR;{
-echo `pwd`
-[ -e ./cgame_mac.bundle ] && zip -9 -r cgame_mac.zip ./cgame_mac.bundle && mv cgame_mac.zip cgame_mac && FILES_TO_COMPRESS=$FILES_TO_COMPRESS" cgame_mac"
-[ -e ./qagame_mac.bundle ] && zip -9 -r qagame_mac.zip ./qagame_mac.bundle && mv qagame_mac.zip qagame_mac && FILES_TO_COMPRESS=$FILES_TO_COMPRESS" qagame_mac"
-[ -e ./ui_mac.bundle ] && zip -9 -r ui_mac.zip ./ui_mac.bundle && mv ui_mac.zip ui_mac && FILES_TO_COMPRESS=$FILES_TO_COMPRESS" ui_mac"
-zip -9 -r mp_bin.zip $FILES_TO_COMPRESS && mv mp_bin.zip mp_bin.pk3
+[ ! -e ./mp_bin ] && mkdir $MPBINDIR
+[ -e ./cgame_mac.bundle ] && zip -9 -r cgame_mac.zip ./cgame_mac.bundle && mv cgame_mac.zip $MPBINDIR/cgame_mac 
+[ -e ./qagame_mac.bundle ] && zip -9 -r qagame_mac.zip ./qagame_mac.bundle && mv qagame_mac.zip $MPBINDIR/qagame_mac 
+[ -e ./ui_mac.bundle ] && zip -9 -r ui_mac.zip ./ui_mac.bundle && mv ui_mac.zip $MPBINDIR/ui_mac 
+zip -9 -r mp_bin.zip $MPBINDIR && mv mp_bin.zip mp_bin.pk3
 }
 popd
